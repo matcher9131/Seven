bf720e901b35cd71a11283685e48e3fc231f7717
matcher9131 <matcher9131cd@gmail.com>
2025-02-04 10:24:10 +0900

    Add tests of Core.Models.Player

---
+++ Seven.Core.Test/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using Seven.Core.Models;
+using Seven.Core.Rules;
+using Seven.Core.Test.TestData;
+
+namespace Seven.Core.Test.Models
+{
+    public class PlayerTest
+    {
+        [Fact]
+        public void NumCardsTest()
+        {
+            var engine = new TestEngine();
+            var game = new Game(Rule.Standard, [engine]);
+            var player = new Player(0b0100000010000_1101000101100_0011100110111_1110000001001UL, game, engine);
+            Assert.Equal(21, player.NumCards);
+        }
+    }
+}
---
+++ Seven.Core.Test/TestData/TestEngine.cs
@@ -0,0 +1,39 @@
+﻿using Seven.Core.Engines;
+using Seven.Core.Models;
+
+namespace Seven.Core.Test.TestData
+{
+    // 常に出せる最小のカードを出すEngine
+    public class TestEngine : EngineBase
+    {
+        public TestEngine() : base("TestEngine")
+        {
+        }
+
+        public override int Next(IReadonlyGame game, IPlayer player)
+        {
+            for (int suit = 0; suit < 4; ++suit)
+            {
+                for (int num = 5; num >= 0; --num)
+                {
+                    int i = 13 * suit + num;
+                    if ((player.Cards & 1UL << i) > 0)
+                    {
+                        return i;
+                    }
+                    if ((game.Board.Cards & 1UL << i) == 0) break;
+                }
+                for (int num = 7; num < 13; ++num)
+                {
+                    int i = 13 * suit + num;
+                    if ((player.Cards & 1UL << i) > 0)
+                    {
+                        return i;
+                    }
+                    if ((game.Board.Cards & 1UL << i) == 0) break;
+                }
+            }
+            return -1;
+        }
+    }
+}
