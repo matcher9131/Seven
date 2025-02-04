c6aca094645faea78bcbb5b024eac392d42f268e
matcher9131 <matcher9131cd@gmail.com>
2025-02-04 10:06:11 +0900

    Add tests of Core.Models.Board

---
+++ Seven.Core.Test/Models/Board.cs
@@ -0,0 +1,22 @@
+﻿using Seven.Core.Models;
+
+namespace Seven.Core.Test.Models
+{
+    public class BoardTest
+    {
+        [Fact]
+        public void ConstructorTest()
+        {
+            Board board = new();
+            Assert.Equal(0b0000001000000_0000001000000_0000001000000_0000001000000UL, board.Cards);
+        }
+
+        [Fact]
+        public void SetBoardTest()
+        {
+            Board board = new();
+            board.SetCard(10);
+            Assert.Equal(0b0000001000000_0000001000000_0000001000000_0010001000000UL, board.Cards);
+        }
+    }
+}
