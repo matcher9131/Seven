7312888abf59aaf5831335931948dd5f59bbfb26
matcher9131 <matcher9131cd@gmail.com>
2025-02-04 11:41:59 +0900

    Add tests of Core.Models.Dealer

---
+++ Seven.Core.Test/Models/Dealer.cs
@@ -0,0 +1,37 @@
+﻿using Seven.Core.Models;
+using Seven.Core.Test.TestDoubles;
+using System.Numerics;
+
+namespace Seven.Core.Test.Models
+{
+    public class DealerTest
+    {
+        [Fact]
+        public void DealTest()
+        {
+            int numPlayers = 5;
+            var random = new FixedRandom() { ReturnValue = 3 };
+            var dealer = new Dealer(random);
+            ulong[] dealtCards = dealer.Deal(numPlayers, true);
+            Assert.Equal(11, BitOperations.PopCount(dealtCards[0]));
+            Assert.Equal(10, BitOperations.PopCount(dealtCards[1]));
+            Assert.Equal(10, BitOperations.PopCount(dealtCards[2]));
+            Assert.Equal(11, BitOperations.PopCount(dealtCards[3]));
+            Assert.Equal(11, BitOperations.PopCount(dealtCards[4]));
+        }
+
+        [Fact]
+        public void DealTest2()
+        {
+            int numPlayers = 5;
+            var random = new FixedRandom() { ReturnValue = 0 };
+            var dealer = new Dealer(random);
+            ulong[] dealtCards = dealer.Deal(numPlayers, false);
+            Assert.Equal(0b0000000000000_0000000000000_0000000000000_0111111111110UL, dealtCards[0]);
+            Assert.Equal(0b0000000000000_0000000000000_0001111111111_1000000000000UL, dealtCards[1]);
+            Assert.Equal(0b0000000000000_0000001111111_1110000000000_0000000000000UL, dealtCards[2]);
+            Assert.Equal(0b0000000001111_1111110000000_0000000000000_0000000000000UL, dealtCards[3]);
+            Assert.Equal(0b1111111110000_0000000000000_0000000000000_0000000000001UL, dealtCards[4]);
+        }
+    }
+}
---
+++ Seven.Core.Test/TestDoubles/FixedRandom.cs
@@ -0,0 +1,9 @@
+﻿namespace Seven.Core.Test.TestDoubles
+{
+    public class FixedRandom : IRandom
+    {
+        public int ReturnValue { get; set; }
+
+        public int Next(int maxValue) => this.ReturnValue;
+    }
+}
