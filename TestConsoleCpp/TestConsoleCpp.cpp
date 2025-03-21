9bf9eaede808817d4ad39b678fc16ae2d69cda78
matcher9131 <matcher9131cd@gmail.com>
2025-03-21 10:48:17 +0900

    Fix counting permutations of card patterns

--- TestConsoleCpp/TestConsoleCpp.cpp
+++ TestConsoleCpp/TestConsoleCpp.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <climits>
 #include <cassert>
+#include <bitset>
 using namespace std;
 using ll = long long;
 
@@ -23,15 +24,20 @@ pair<int, int> getTopTwo(int pattern) {
 }
 
 int main() {
-    // 対象となるパターン（カードあり and 7に近い2枚が階段ではない）の頂点集合
+    // 対象となるパターン（2枚以上のカードあり and 7に近い2枚が階段ではない）の頂点集合
+    // ※計算量の都合で単体はあとから差し込む
     vector<int> vertexes;
     for (int i = 1; i < VERTEX_MAX; ++i) {
         auto [first, second] = getTopTwo(i);
-        if (second == -1 || first - second > 1) {
+        if (second >= 0 && first - second > 1) {
             vertexes.push_back(i);
         }
     }
     int numVertexes = vertexes.size();
+
+    for (const int v : vertexes) {
+        cout << bitset<6>(v) << endl;
+    }
 
     // 2つのパターンの優先度に順位をつけるための辺集合（uが優先度高）
     // 後の操作で計算量を減らすためにu,vは実際の頂点番号ではなくvertexes内のインデックスにする
@@ -44,15 +50,23 @@ int main() {
             // 以降x < yに留意
             if (x1 == y1 && x2 == y2 && (x & y) == x) {
                 // 7に近い2枚が同じで、 全体が包含関係にあるパターン（枚数が多いほうが優先度高）
+                // 例：010100と010110
                 edges.emplace(j, i);
             } else if ((x ^ 1 << x1) == (y ^ 1 << y1)) {
                 // 7に最も近いカードのみが異なり、以降が全く同じパターン（7から遠いほうが優先度高）
+                // 例：100101と010101
                 edges.emplace(i, j);
             } else {
-                // スライドすれば一致するパターン（7から遠いほうが優先度高）
                 for (int bit = 1; bit < BIT_WIDTH; ++bit) {
+                    // スライドすれば一致するパターン（7から遠いほうが優先度高）
+                    // 例：100100と010010
                     if (y == (x << bit)) {
                         edges.emplace(i, j);
+                    }
+                    // スライドして一致かつ右端から連続してカードを持っているパターン（枚数が多いほうが優先度高）
+                    // 例：000101と010111
+                    if (y == ((x << bit) | ((1 << bit) - 1))) {
+                        edges.emplace(j, i);
                     }
                 }
             }
@@ -98,3 +112,11 @@ int main() {
 
     return 0;
 }
+
+// 現状、単体を除いても34,538,776,845,694,347通り
+
+// スライド＋最右以降が全てカードあり
+// 001010 弱
+// 010111 強
+
+// これでも 24,990,225,757,718通り
