173d8148b948a0a1c11b2dbb5dea9f62b49e4106
matcher9131 <matcher9131cd@gmail.com>
2025-03-24 11:20:36 +0900

    Add tests of Helper.OrderBasedCrossover

--- Seven.Test/HelperTest.cs
+++ Seven.Test/HelperTest.cs
@@ -2,9 +2,36 @@
 {
     public class HelperTest
     {
-        public class HelperTestData : TheoryData<int[], int, int, int, int[]>
+        public class OrderBasedCrossoverTestData : TheoryData<int[], int[], int[], int[]>
         {
-            public HelperTestData()
+            public OrderBasedCrossoverTestData()
+            {
+                this.Add(
+                    [3, 4, 1, 2, 5, 8, 7, 6],
+                    [2, 5, 4, 6, 1, 7, 3, 8],
+                    [0, 2, 5, 7],
+                    [2, 5, 4, 3, 1, 7, 8, 6]
+                );
+                this.Add(
+                    [4, 5, 1, 7, 6, 3, 2, 0],
+                    [5, 4, 0, 1, 2, 6, 7, 3],
+                    [2, 5, 6, 7],
+                    [5, 4, 1, 3, 2, 6, 7, 0]
+                );
+            }
+        }
+
+        [Theory]
+        [ClassData(typeof(OrderBasedCrossoverTestData))]
+        public void OrderBasedCrossoverTest(int[] x, int[] y, int[] indices, int[] expected)
+        {
+            var actual = Helper.OrderBasedCrossover(x, y, indices);
+            Assert.Equal(expected, actual);
+        }
+
+        public class CutAndInsertTestData : TheoryData<int[], int, int, int, int[]>
+        {
+            public CutAndInsertTestData()
             {
                 this.Add([0, 1, 2, 3, 4, 5, 6], 2, 6, 1, [0, 2, 3, 4, 5, 1, 6]);
                 this.Add([0, 1, 2, 3, 4, 5, 6], 2, 6, 3, [0, 1, 6, 2, 3, 4, 5]);
@@ -15,7 +42,7 @@
         }
 
         [Theory]
-        [ClassData(typeof(HelperTestData))]
+        [ClassData(typeof(CutAndInsertTestData))]
         public void CutAndInsertTest(int[] perm, int l, int r, int i, int[] expected)
         {
             var actual = Helper.CutAndInsert(perm, l, r, i);
