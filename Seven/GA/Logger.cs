7f7361f09da6a4d2e7db4b4ba2ee0e71ce81d2ca
matcher9131 <matcher9131cd@gmail.com>
2025-03-26 11:49:00 +0900

    Add GA.Logger

---
+++ Seven/GA/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.GA
+{
+    public class Logger
+    {
+        public void Log(string content)
+        {
+            string fileName = $"{DateTime.Now:yyyyMMddHHmmss}.log";
+            File.WriteAllText(fileName, content);
+        }
+    }
+}
