3728a0220b267b17188e6fcbfd483894b02f4957
matcher9131 <matcher9131cd@gmail.com>
2025-03-27 10:49:53 +0900

    Add ThreadLocalBenchmark

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -2,4 +2,5 @@
 using BenchmarkDotNet.Running;
 
 // var summary = BenchmarkRunner.Run<TakeSelectBenchmark>();
-var summary = BenchmarkRunner.Run<OrderBasedCrossoverBenchmark>();
+// var summary = BenchmarkRunner.Run<OrderBasedCrossoverBenchmark>();
+var summary = BenchmarkRunner.Run<ThreadLocalBenchmark>();
---
+++ Benchmark/ThreadLocalBenchmark.cs
@@ -0,0 +1,49 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmark
+{
+    public class ThreadLocalBenchmark
+    {
+        public class Engine(Random random)
+        {
+            private readonly Random random = random;
+            public int Next() => this.random.Next();
+        }
+
+        public class EngineThreadLocal()
+        {
+            private readonly ThreadLocal<Random> random = new();
+
+            public void SetRandom(Random random) => this.random.Value = random;
+            public int Next() => this.random.Value?.Next() ?? throw new InvalidOperationException();
+        }
+
+        const int N = 100000;
+
+        [Benchmark]
+        public long Evaluate()
+        {
+            return Enumerable.Range(0, N).AsParallel().Sum(_ =>
+            {
+                Engine engine = new(new Random(Random.Shared.Next()));
+                return (long)engine.Next();
+            });
+        }
+
+        [Benchmark]
+        public long EvaluateThreadLocal()
+        {
+            EngineThreadLocal engine = new();
+            return Enumerable.Range(0, N).AsParallel().Sum(_ =>
+            {
+                engine.SetRandom(new Random(Random.Shared.Next()));
+                return (long)engine.Next();
+            });
+        }
+    }
+}
